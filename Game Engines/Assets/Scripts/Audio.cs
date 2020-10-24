c795a4cb35587b879228a09667aefdb6c35adba6
EmpireofZakuul <60693663+EmpireofZakuul@users.noreply.github.com>
2020-10-24 15:29:05 +0100

    Added the Audio script

---
+++ Game Engines/Assets/Scripts/Audio.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class Audio : MonoBehaviour
+{
+    private AudioSource audioSource;
+    public static float[] AudioSamples = new float[512];
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AudioSpectrum();
+        //audioSource.GetSpectrumData(AudioSamples, 0, FFTWindow.Blackman);
+    }
+
+    public void AudioSpectrum()
+    {
+        audioSource.GetSpectrumData(AudioSamples, 0, FFTWindow.Blackman);
+    }
+}
