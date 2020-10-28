9e7cde3028bb04c0ccda792191e0fbca700242f5
EmpireofZakuul <60693663+EmpireofZakuul@users.noreply.github.com>
2020-10-28 16:36:46 +0000

    Pause
    Added a script which allows the player to pause and upause the game with the escape button. when the escape button is pressed the menu will pop up which will allow the player to navigate.

---
+++ Game Engines/Assets/Scripts/PauseGame.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseGame : MonoBehaviour
+{
+    //InputManager inputManager;
+
+    
+   // public GameObject Keys;
+   // public GameObject SettingsGame;
+    public bool Paused = false;
+    public GameObject PauseMenuUI;
+   // public GameObject health;
+   // public GameObject keymenu;
+   // public GameObject menu;
+    //public GameObject settings;
+
+    public void Awake()
+    {
+        Pause();
+    }
+    public void Start()
+    {
+        //inputManager = GameObject.FindObjectOfType<InputManager>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (Paused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+
+    }
+
+    public void Resume()
+    {
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        Paused = false;
+        AudioListener.pause = false;
+        //  health.SetActive(true);
+        // keymenu.SetActive(false);
+        // menu.SetActive(false);
+        // settings.SetActive(false);
+
+    }
+
+    public void Pause()
+    {
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        Paused = true;
+        //health.SetActive(false);
+        AudioListener.pause = true;
+    }
+
+    public void QuitGameMenu()
+    {
+
+#if UNITY_STANDALONE
+        Application.Quit();
+#endif
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+
+}
