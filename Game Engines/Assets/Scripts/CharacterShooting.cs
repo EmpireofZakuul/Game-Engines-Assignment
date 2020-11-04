1fd08b98f78ae35548c832ac7950f950376204ff
EmpireofZakuul <60693663+EmpireofZakuul@users.noreply.github.com>
2020-11-04 19:28:00 +0000

    Have Player shooting working

--- Game Engines/Assets/Scripts/CharacterShooting.cs
+++ Game Engines/Assets/Scripts/CharacterShooting.cs
@@ -6,15 +6,17 @@ public class CharacterShooting : MonoBehaviour
 {
     public float fireRate = 15f;
     private float nextTimeToFire = 0f;
-    public Rigidbody projectile;
+    //public Rigidbody projectile;
+    public GameObject projectile;
+    public Transform bulletSpawn;
     public float speed = 20;
-    public AudioSource source;
-    public AudioClip shot;
+   // public AudioSource source;
+   // public AudioClip shot;
 
 
     public void Start()
     {
-        source = GetComponent<AudioSource>();
+        //source = GetComponent<AudioSource>();
 
     }
 
@@ -30,8 +32,15 @@ public class CharacterShooting : MonoBehaviour
     }
     public void Shoot()
     {
-        source.Play();
-        Rigidbody instantiatedProjectile = Instantiate(projectile, transform.position, transform.rotation) as Rigidbody;
-        instantiatedProjectile.velocity = transform.TransformDirection(new Vector3(0, 0, speed));
+        //source.Play();
+        //Rigidbody instantiatedProjectile = Instantiate(projectile, this.transform.position, transform.rotation) as Rigidbody;
+        //instantiatedProjectile.velocity = transform.TransformDirection(new Vector3(0, 0, speed));
+        GameObject bullet = Instantiate(projectile);
+        bullet.transform.position = bulletSpawn.position;
+        Quaternion rotation = transform.rotation;
+        // Vector3 rotation = bullet.transform.rotation.eulerAngles;
+        // bullet.transform.rotation = Quaternion.Euler(rotation.x, transform.eulerAngles.y, rotation.z);
+        bullet.GetComponent<Rigidbody>().AddForce(bulletSpawn.right * speed, ForceMode.Impulse);
+
     }
 }
---
+++ Game Engines/Assets/bulletDie.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletDie : MonoBehaviour
+{
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(gameObject.tag == "Enemy")
+        {
+            Destroy(other.gameObject, 1f);
+        }
+        else
+        {
+            Destroy(gameObject, 2f);
+        }
+    }
+}
