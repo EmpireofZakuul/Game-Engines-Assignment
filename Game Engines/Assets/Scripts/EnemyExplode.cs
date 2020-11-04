3ac2aaca5bc4fe53efb3bb611225d3610746ae49
EmpireofZakuul <60693663+EmpireofZakuul@users.noreply.github.com>
2020-11-04 21:07:14 +0000

    added sound effects and created a corountine
    created a corountine that goes through a few sound effects before the enemy destroys itself

--- Game Engines/Assets/Scripts/EnemyExplode.cs
+++ Game Engines/Assets/Scripts/EnemyExplode.cs
@@ -9,13 +9,16 @@ public class EnemyExplode : MonoBehaviour
 
     public bool explode = false;
     public AudioSource source;
-    public AudioClip shot;
+    public AudioClip[] clipsAudio;
     public GameObject theObjectToBeUnParented;
     // Start is called before the first frame update
     void OnEnable()
     {
         enemyHealth = GetComponentInParent<EnemyHealth>();
         source = GetComponent<AudioSource>();
+        
+        source.clip = clipsAudio[0];
+        source.Play();
     }
 
     // Update is called once per frame
@@ -23,16 +26,25 @@ public class EnemyExplode : MonoBehaviour
     {
         if (EnemyHealth.health <= 0 && !explode)
         {
-            source.Play();
-            theObjectToBeUnParented.transform.SetParent(null);
-            Instantiate(effect, transform.position, transform.rotation);
+
+            StartCoroutine(ExampleCoroutine());
             explode = true;
-            Destroy(gameObject,0.5f);
         }
     }
 
-  
-    
-        
-    
+    IEnumerator ExampleCoroutine()
+    {
+        theObjectToBeUnParented.transform.SetParent(null);
+        yield return new WaitForSeconds(2);
+        source.clip = clipsAudio[1];
+        source.Play();
+        yield return new WaitForSeconds(2);
+        source.clip = clipsAudio[2];
+        source.Play();
+        Instantiate(effect, transform.position, transform.rotation);
+        Destroy(gameObject, 0.9f);
+
+
+
+    }
 }
