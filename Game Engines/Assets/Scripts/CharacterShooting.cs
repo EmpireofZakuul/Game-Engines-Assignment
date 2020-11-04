ed6e2e5f0b889c75f1021c2da8970f255dcf4c31
EmpireofZakuul <60693663+EmpireofZakuul@users.noreply.github.com>
2020-11-04 17:05:01 +0000

    Fixed gun in blender, started shooting script

--- Game Engines/Assets/Scripts/CharacterShooting.cs
+++ Game Engines/Assets/Scripts/CharacterShooting.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class CharacterShooting : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float fireRate = 15f;
+    private float nextTimeToFire = 0f;
+    public Rigidbody projectile;
+    public float speed = 20;
+    public AudioSource source;
+    public AudioClip shot;
+
+
+    public void Start()
     {
-        
+        source = GetComponent<AudioSource>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
+        {
+            nextTimeToFire = Time.time + 1 / fireRate;
+            Shoot();
+        }
+
+    }
+    public void Shoot()
+    {
+        source.Play();
+        Rigidbody instantiatedProjectile = Instantiate(projectile, transform.position, transform.rotation) as Rigidbody;
+        instantiatedProjectile.velocity = transform.TransformDirection(new Vector3(0, 0, speed));
     }
 }
