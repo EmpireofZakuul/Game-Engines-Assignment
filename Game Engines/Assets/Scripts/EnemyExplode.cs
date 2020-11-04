0b2fa57c187d311e8cd39caa5c57d4913e22b5bc
EmpireofZakuul <60693663+EmpireofZakuul@users.noreply.github.com>
2020-11-04 20:01:26 +0000

    when the enemies health reaches 0, the enemies leg is unparented from the body

--- Game Engines/Assets/Scripts/EnemyExplode.cs
+++ Game Engines/Assets/Scripts/EnemyExplode.cs
@@ -10,8 +10,9 @@ public class EnemyExplode : MonoBehaviour
     public bool explode = false;
     public AudioSource source;
     public AudioClip shot;
+    public GameObject theObjectToBeUnParented;
     // Start is called before the first frame update
-    void Start()
+    void OnEnable()
     {
         enemyHealth = GetComponentInParent<EnemyHealth>();
         source = GetComponent<AudioSource>();
@@ -23,9 +24,15 @@ public class EnemyExplode : MonoBehaviour
         if (EnemyHealth.health <= 0 && !explode)
         {
             source.Play();
+            theObjectToBeUnParented.transform.SetParent(null);
             Instantiate(effect, transform.position, transform.rotation);
             explode = true;
-            Destroy(gameObject,0.1f);
+            Destroy(gameObject,0.5f);
         }
     }
+
+  
+    
+        
+    
 }
